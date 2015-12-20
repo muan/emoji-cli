dbefea987cc3fe54714e200ba4ba0718efbe8d1a
Mu-An Chiou <me@muanchiou.com>
2015-12-20 16:26:46 +0900

    :m: mtest

--- mrblib/emoji-cli.rb
+++ mrblib/emoji-cli.rb
@@ -2,25 +2,9 @@ def __main__(argv)
   if argv[1] == "version"
     puts "v#{EmojiCli::VERSION}"
   else
-    if argv[1] && result = EmojiCli::DATA[argv[1].downcase]
-      emoji = if result.length > 1
-        # if -r return a random emoji from result
-        if argv.select{|arg| ["-r", "--random"].include? arg }.any?
-          result[rand(result.length)]
-        # else print result and get user input
-        else
-          i = 0
-          puts "#{result.map{|m| (i = i + 1).to_s + ". " + m}.join("\n")}"
-          print "Choose [1~#{result.length}] "
-          input = gets.strip
-          (input.length > 0 && result[input.to_i - 1]) || result[0]
-        end
-      else
-        result[0]
-      end
-
+    if result = search(argv[1])
       # copy emoji and notify
-      copy emoji
+      copy choose_emoji(result, argv.select{|arg| ["-r", "--random"].include? arg }.any?)
       puts "Copied #{emoji} !"
     else
       puts "Emoji not found 😭"
@@ -28,12 +12,34 @@ def __main__(argv)
   end
 end
 
+def search keyword
+  EmojiCli::DATA[keyword.downcase]
+end
+
+def which_emoji result, random
+  if result.length > 1
+    # if -r return a random emoji from result
+    if random
+      result[rand(result.length)]
+    # else print result and get user input
+    else
+      i = 0
+      puts "#{result.map{|m| (i = i + 1).to_s + ". " + m}.join("\n")}"
+      print "Choose [1~#{result.length}] "
+      input = gets.strip
+      (input.length > 0 && result[input.to_i - 1]) || result[0]
+    end
+  else
+    result[0]
+  end
+end
+
 def copy emoji
   os = `uname`.strip
-  cmd = if os == "Darwin"
-    "echo '#{emoji}' | tr -d '\n' | pbcopy"
-  else
-    "echo '#{emoji}' | tr -d '\n' | xclip -i"
-  end
-  `#{cmd}`
+  # cmd = if os == "Darwin"
+  #   "echo '#{emoji}' | tr -d '\n' | pbcopy"
+  # else
+  #   "echo '#{emoji}' | tr -d '\n' | xclip -i"
+  # end
+  # `#{cmd}`
 end
--- test/test_emoji-cli.rb
+++ test/test_emoji-cli.rb
@@ -1,6 +1,13 @@
 class TestEmojiCli < MTest::Unit::TestCase
-  def test_main
-    assert_nil __main__([])
+  def test_search
+    assert_equal search("poop"), ["💩"]
+  end
+
+  def test_which_emoji
+    assert_equal which_emoji(["💩"], false), "💩"
+    assert_block "when random is on just return a random one" do
+      ["💩", "🎉"].include? which_emoji(["💩", "🎉"], true)
+    end
   end
 end
 
